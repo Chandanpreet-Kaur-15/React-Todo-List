100fddda4623905c20889c4feb96edd878fe2018
Chandanpreet-Kaur-15 <5141chand@gmail.com>
2024-08-09 12:43:04 -0700

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import TodoList from "./Components/Todo List"
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-slate-900 min-h-screen">
+      <TodoList />
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Todo List/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function TodoList() {
+  return (
+    <>
+    <div className='text-red-800'>TodoList</div>
+    </>
+  )
+}
+
+export default TodoList
